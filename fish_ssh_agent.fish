609cdcff0bdbc518c677560137870d821a5254e6
Ike Johnson-Woods <contact@ike.id.au>
2021-11-23 17:12:05 +0800

    Use good ol' original Fish SSH agent

--- fish_ssh_agent.fish
+++ fish_ssh_agent.fish
@@ -1,8 +1,32 @@
-function fish_ssh_agent -d "Set SSH agent to Windows SSH agent"
-  export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-  set ALREADY_RUNNING (ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; echo $status)
-  if test "$ALREADY_RUNNING" -gt 0
-    rm -f $SSH_AUTH_SOCK
-    setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"/mnt/c/PATH/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &
-  end
+function __ssh_agent_is_started -d "check if ssh agent is already started"
+   if begin; test -f $SSH_ENV; and test -z "$SSH_AGENT_PID"; end
+      source $SSH_ENV > /dev/null
+   end
+
+   if test -z "$SSH_AGENT_PID"
+      return 1
+   end
+
+   ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep -q ssh-agent
+   #pgrep ssh-agent
+   return $status
+end
+
+
+function __ssh_agent_start -d "start a new ssh agent"
+   ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+   chmod 600 $SSH_ENV
+   source $SSH_ENV > /dev/null
+   true  # suppress errors from setenv, i.e. set -gx
+end
+
+
+function fish_ssh_agent --description "Start ssh-agent if not started yet, or uses already started ssh-agent."
+   if test -z "$SSH_ENV"
+      set -xg SSH_ENV $HOME/.ssh/environment
+   end
+
+   if not __ssh_agent_is_started
+      __ssh_agent_start
+   end
 end
